37fc912c1dc31601fad3bb08968184d4c7d2c857
Richard Jiang <Richard Jiang>
2017-05-11 14:09:55 -0700

    Completed Exercise

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,11 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+scatterplot <- function(df, xaxis, yaxis, color, ptitle = "Title", xlabel = "X Title", ylabel = "Y Title") {
+  library(ggplot2)
+  plot <- ggplot(df) +
+    geom_point(mapping = aes(x = df[, xaxis], y = df[, yaxis], color = df[, color])) +
+    labs(x = xlabel, y = ylabel, title = ptitle, color = color)
+  return (plot)
+}
