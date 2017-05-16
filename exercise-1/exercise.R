55b5e3c637b034d47327689dc0f581dd489d0602
Richard Jiang <Richard Jiang>
2017-05-16 14:49:59 -0700

    Completed Exercise

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,11 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd('~/OneDrive/Documents/INFO 201/m18-shiny/exercise-1')
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source('scripts/BuildScatter.R')
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+plot <- scatterplot(iris, 'Sepal.Length', 'Petal.Length', 'Species', 'Iris Dataset', 'Sepal Length', 'Petal Length')
+plot
--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -2,6 +2,8 @@
 shinyUI(fluidPage(
   mainPanel(
     # Add a selectInput (with a proper id) that allows you to select a variable to map
+    selectInput('var', label = h3('Variable'),
+                choices = list('Population' = 'population', 'Votes' = 'votes', 'Ratio' = 'ratio')),
     
     # Use plotlyOutput to show your map
     plotlyOutput('map')
